ac997e49de056f9e36b6aa355e3eb43881768bcf
Axel Hodler <axel@hodler.co>
2018-10-11 07:34:46 +0200

    picking part of parametrized

--- src/test/kotlin/co/hodler/hang/HangmanShould.kt
+++ src/test/kotlin/co/hodler/hang/HangmanShould.kt
@@ -10,8 +10,24 @@ import java.util.stream.Stream
 class HangmanShould {
 
     private fun testDataProvider() = Stream.of(
-            HangmanTestData(input = "word", expectedOutput = "____"),
-            HangmanTestData(input = "way", expectedOutput = "___")
+            HangmanTestData(
+                    input = "word",
+                    expectedOutput = "____"
+            ),
+            HangmanTestData(
+                    input = "way",
+                    expectedOutput = "___"
+            ),
+            HangmanTestData(
+                    input = "bear",
+                    picks = listOf('e'),
+                    expectedOutput = "_e__"
+            ),
+            HangmanTestData(
+                    input = "tree",
+                    picks = listOf('r', 't'),
+                    expectedOutput = "tr__"
+            )
     )
 
     @ParameterizedTest
@@ -19,11 +35,14 @@ class HangmanShould {
     fun `display word, without found characters, with underscores`(testData: HangmanTestData) {
         val subject = Hangman(wordToGuess = testData.input)
 
+        testData.picks.forEach { subject.pick(it) }
+
         assertThat(subject.display()).isEqualTo(testData.expectedOutput)
     }
 
     data class HangmanTestData(
             val input: String,
+            val picks: List<Char> = emptyList(),
             val expectedOutput: String
     )
 
@@ -31,6 +50,14 @@ class HangmanShould {
 
 class Hangman(val wordToGuess: String) {
     fun display(): String {
-        return wordToGuess.map { "_" }.joinToString(separator = "")
+        return wordToGuess
+                .map { if (pickedChars.contains(it)) it else "_" }
+                .joinToString(separator = "")
+    }
+
+    private var pickedChars = mutableListOf<Char>()
+
+    fun pick(letter: Char) {
+        this.pickedChars.add(letter);
     }
 }
