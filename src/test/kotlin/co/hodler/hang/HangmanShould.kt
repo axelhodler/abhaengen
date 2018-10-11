90fff6397d8835e531debada84230c00d5056d03
Axel Hodler <axel@hodler.co>
2018-10-11 07:24:16 +0200

    add parametrization

--- src/test/kotlin/co/hodler/hang/HangmanShould.kt
+++ src/test/kotlin/co/hodler/hang/HangmanShould.kt
@@ -1,17 +1,32 @@
 package co.hodler.hang
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class HangmanShould {
-    @Test
-    fun `display initial word, without yet found characters, with underscores`() {
-        val first = Hangman(wordToGuess = "word")
-        val second = Hangman(wordToGuess = "way")
 
-        assertThat(first.display()).isEqualTo("____")
-        assertThat(second.display()).isEqualTo("___")
+    private fun testDataProvider() = Stream.of(
+            HangmanTestData(input = "word", expectedOutput = "____"),
+            HangmanTestData(input = "way", expectedOutput = "___")
+    )
+
+    @ParameterizedTest
+    @MethodSource("testDataProvider")
+    fun `display word, without found characters, with underscores`(testData: HangmanTestData) {
+        val subject = Hangman(wordToGuess = testData.input)
+
+        assertThat(subject.display()).isEqualTo(testData.expectedOutput)
     }
+
+    data class HangmanTestData(
+            val input: String,
+            val expectedOutput: String
+    )
+
 }
 
 class Hangman(val wordToGuess: String) {
