122d73bfa952e74a89c151d9284dd51bbcdc6098
Axel Hodler <axel@hodler.co>
2018-10-16 09:50:14 +0200

    add game dynamics

--- src/main/kotlin/co/hodler/hang/DefaultGameService.kt
+++ src/main/kotlin/co/hodler/hang/DefaultGameService.kt
@@ -4,9 +4,15 @@ import org.springframework.stereotype.Service
 
 @Service
 class DefaultGameService(val gameRepository: GameRepository) : GameService {
+    private var gameIdCounter = 1
     override fun startGame(): GameStatus {
+        val game = gameRepository.saveGame(
+                id = gameIdCounter.toString(),
+                pickedLetters = emptyList()
+        )
+        gameIdCounter++
         return GameStatus(
-                id = "1",
+                id = game.id,
                 placeholder = Hangman(wordToGuess = "tree").display()
         )
     }
@@ -23,7 +29,7 @@ class DefaultGameService(val gameRepository: GameRepository) : GameService {
 interface GameRepository {
     fun findGameById(id: String): Game
 
-    fun saveGame(id: String, pickedLetters: List<Char>)
+    fun saveGame(id: String, pickedLetters: List<Char>): Game
 }
 
 data class Game(val id: String,
--- src/test/kotlin/co/hodler/hang/DefaultGameServiceTest.kt
+++ src/test/kotlin/co/hodler/hang/DefaultGameServiceTest.kt
@@ -1,40 +1,48 @@
 package co.hodler.hang
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 import org.springframework.stereotype.Repository
 
+@Tag("unit")
 internal class DefaultGameServiceTest {
-    @Test
-    fun `finds game by id`() {
-        val subject = DefaultGameService(InMemoryGameRepository())
-        val gamestatus = subject.playGame("2")
+    private val subject = DefaultGameService(InMemoryGameRepository())
 
-        assertThat(gamestatus.id).isEqualTo("2")
+    @Test
+    fun `stores game`() {
+        subject.startGame()
+        val gamestatus = subject.playGame("1")
+
+        assertThat(gamestatus.id).isEqualTo("1")
         assertThat(gamestatus.placeholder).isEqualTo("_r__")
     }
 
     @Test
-    fun `stores game`() {
-        val subject = DefaultGameService(InMemoryGameRepository())
-        val gamestatus = subject.startGame()
+    fun `stores games`() {
+        subject.startGame()
+        subject.startGame()
+        subject.startGame()
+        val gamestatus = subject.playGame("3")
 
-        assertThat(gamestatus.id).isEqualTo("1")
-        assertThat(gamestatus.placeholder).isEqualTo("____")
+        assertThat(gamestatus.id).isEqualTo("3")
+        assertThat(gamestatus.placeholder).isEqualTo("_r__")
     }
 }
 
 @Repository
 class InMemoryGameRepository : GameRepository {
-    override fun saveGame(id: String, pickedLetters: List<Char>) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    private var games = mutableListOf<Game>()
+
+    override fun saveGame(id: String, pickedLetters: List<Char>): Game {
+        val game = Game(id, "tree", pickedLetters)
+        games.add(game)
+        return game
     }
 
     override fun findGameById(id: String): Game {
-        return Game(
-                id = id,
-                originalWord = "tree",
-                pickedLetters = listOf('r')
-        )
+        val game = games.find { it.id == id }
+        return Game(game!!.id, game!!.originalWord,
+                listOf('r'))
     }
 }
